abeb24dbdb822b00e9a8f2a89cf0aaad248b4411
ILFT <ftilmtlk@gmail.com>
2022-10-21 12:07:29 +0300

    0.0.3 next create parsing test

--- model/company.py
+++ model/company.py
@@ -16,12 +16,10 @@ class Company :
     equity: float
     liabilities: float
     assets: float
-    bookValueOfNetAssets: float
-    earningsGrowRate: float
 
 
 
-    def __init__(self, name: str, marketCap: float, earnings: float, revenue: float, cashFlow: float, equity: float, liabilities: float, assets: float, bookValueOfNetAssets: float, earningsGrowRate: float):
+    def __init__(self, name: str, marketCap: float, earnings: float, revenue: float, cashFlow: float, equity: float, liabilities: float, assets: float):
         self.name = name
         self.marketCap =  marketCap
         self.earnings = earnings
@@ -30,14 +28,9 @@ class Company :
         self.equity = equity
         self.liabilities = liabilities
         self.assets = assets
-        self.bookValueOfNetAssets = bookValueOfNetAssets
-        self.earningsGrowRate = earningsGrowRate
     
     def get_price_to_earnings(self) -> float:
         return self.marketCap / self.earnings
-
-    def get_price_earnings_growth_ratio(self) -> float:
-        return self.marketCap / self.earnings / self.earningsGrowRate
 
     def get_price_to_sales_ratio(self) -> float:
         return self.marketCap / self.revenue
@@ -46,7 +39,7 @@ class Company :
         return self.marketCap / self.cashFlow
     
     def get_price_to_book_value(self) -> float:
-        return self.marketCap / self.bookValueOfNetAssets
+        return self.marketCap / self.equity
     
     def get_debt_to_equity_ratio(self) -> float:
         return self.liabilities / self.equity
--- test.py
+++ test.py
@@ -1,9 +1,33 @@
 
-from unicodedata import name
+from bs4 import BeautifulSoup 
+import re 
 import requests
 
 
 if __name__ == "__main__":
 
-    r = requests.get("https://smart-lab.ru/q/aas/f")
-    print(r.status_code)
+    response  = requests.get("https://smart-lab.ru/q/tcsg/f")
+    print(response.status_code)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    marketCap = soup.find('tr', field = re.compile('\S*market_cap')).findChildren('td')[5].text
+    print(marketCap)
+
+    earnings = soup.find('tr', field = re.compile('\S*net_income')).findChildren('td')[5].text
+    print(earnings)
+
+    #revenue = soup.find('tr', field = re.compile('\S*revenue')).findChildren('td')[5].text
+    #print(revenue)
+
+    #cashFlow = soup.find('tr', field = re.compile('\S*ocf')).findChildren('td')[5].text
+    #print(cashFlow)
+
+    equity = soup.find('tr', field = re.compile('\S*cash')).findChildren('td')[5].text
+    print(equity)
+
+    liabilities = soup.find('tr', field = re.compile('\S*debt')).findChildren('td')[5].text
+    print(liabilities)
+
+    assets = soup.find('tr', field = re.compile('\S*assets')).findChildren('td')[5].text
+    print(assets)
