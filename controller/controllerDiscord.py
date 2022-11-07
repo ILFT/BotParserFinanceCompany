d4c4a06daa1415a2a64cd80abf8af3d2f66b804d
ILFT <ftilmtlk@gmail.com>
2022-11-07 13:06:10 +0300

    0.0.9 search function

--- controller/controllerDiscord.py
+++ controller/controllerDiscord.py
@@ -1,7 +1,6 @@
 from controller.controllerApp import ControllerApp
 from discord.ext.commands import Bot
 from discord.ext.commands import Context
-from discord.ext.commands import Cog
 from discord.ext import commands
 from discord import Intents
 
@@ -17,28 +16,36 @@ class ControllerDiscord():
     bot: Bot
     observer: ViewDiscord
 
-    def __init__(self ,prefix: str):
+    def __init__(self, prefix: str):
         self.controller = ControllerApp()
         self.bot = Bot(command_prefix = prefix, intents = Intents.all())
         self.observer = ViewDiscord()
         self.__set_handler_bot()
+
+
     """
     @bot.event
     async def on_ready():
         print('BOT connected')
     """
+    
 
     @commands.command()
-    async def search(self, ctx: Context):
-        await ctx.send('fff')
+    #@self.bot.command()
+    async def search(ctx: Context, arg):
+        #print(ctx.author)
+        #print(ctx.message.content)
+        print(arg)
+        await ctx.send(arg)
         #await self.observer.send_search_company(ctx, self.controller.execute_command(ctx.command, arg))
 
-
+    
     def start_bot(self, token: str):
         self.bot.run(token)
 
     def stop_bot(self):
         self.bot.close()
-
+    
     def __set_handler_bot(self):
         self.bot.add_command(self.search)
+    
